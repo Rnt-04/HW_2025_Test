65316f0f4d78b97daf70a88fd7c747bab36a5d3c
Rnt-04 <ritintagore@gmail.com>
2025-11-29 04:17:18 +0530

    GameScene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float fallDeathY = -2f;   // optional: for death on falling
+
+    private float moveSpeed = 3f;
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        if (ConfigManager.Instance != null)
+            moveSpeed = ConfigManager.Instance.PlayerSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(h, 0f, v).normalized;
+        Vector3 vel = rb.linearVelocity;
+        vel.x = dir.x * moveSpeed;
+        vel.z = dir.z * moveSpeed;
+        rb.linearVelocity = vel;
+
     }
 }
